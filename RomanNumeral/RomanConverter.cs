6aaa101a54914c303d6e32a981c5dc6838cc3798
Sam Skinner <sam.sjskinner@gmail.com>
2021-03-22 18:51:34 +1100

    Can now convert the integer 1 to the string "I"

--- RomanNumeral/RomanConverter.cs
+++ RomanNumeral/RomanConverter.cs
@@ -2,8 +2,12 @@ namespace RomanNumeral
 {
     public class RomanConverter
     {
-        public string Convert()
+        public string Convert(int input)
         {
+            if (input == 1)
+            {
+                return "I";
+            }
             return "";
         }
     }
--- RomanNumeralTests/RomanConverterTests.cs
+++ RomanNumeralTests/RomanConverterTests.cs
@@ -6,12 +6,25 @@ namespace RomanNumeralTests
     public class RomanConverterTests
     {
         [Fact]
-        public void GivenConvert_WhenInputIsNull_ThenReturnEmptyString()
+        public void GivenConvert_WhenInputIsZero_ThenReturnEmptyString()
         {
             RomanConverter converter = new RomanConverter();
+            int input = 0;
             string expected = "";
 
-            string actual = converter.Convert();
+            string actual = converter.Convert(input);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GivenConvert_WhenInputIs1_ThenReturnStringI()
+        {
+            RomanConverter converter = new RomanConverter();
+            int input = 1;
+            string expected = "I";
+
+            string actual = converter.Convert(input);
 
             Assert.Equal(expected, actual);
         }
