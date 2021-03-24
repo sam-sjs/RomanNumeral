499445e3600b8915ba8df8b14e9477ddb7f92520
Sam Skinner <sam.sjskinner@gmail.com>
2021-03-24 22:47:07 +1100

    Restarting because I didn't plan properly, what the heck it's extra practice right...

--- RomanNumeral/RomanConverter.cs
+++ RomanNumeral/RomanConverter.cs
@@ -4,33 +4,6 @@ namespace RomanNumeral
 {
     public class RomanConverter
     {
-        public string Convert(int input)
-        {
-            var numeralMapping = new Dictionary<int, string>
-            {
-                {0, ""},
-                {1, "I"},
-                {5, "V"},
-                {10, "X"},
-                {50, "L"},
-                {100, "C"},
-                {500, "D"},
-                {1000, "M"}
-            };
-            return numeralMapping[input];
-        }
-
-        public int SplitNumeralValues(int input)
-        {
-            return input switch
-            {
-                < 10 => input,
-                < 50 => input / 10,
-                < 100 => input / 50,
-                < 500 => input / 100,
-                < 1000 => input / 500,
-                _ => input / 1000
-            };
-        }
+       
     }
 }
--- RomanNumeralTests/RomanConverterTests.cs
+++ RomanNumeralTests/RomanConverterTests.cs
@@ -5,39 +5,6 @@ namespace RomanNumeralTests
 {
     public class RomanConverterTests
     {
-        [Theory]
-        [InlineData(0, "")]
-        [InlineData(1, "I")]
-        [InlineData(5, "V")]
-        [InlineData(10, "X")]
-        [InlineData(50, "L")]
-        [InlineData(100, "C")]
-        [InlineData(500, "D")]
-        [InlineData(1000, "M")]
-        public void Convert_WhenGivenInt_ReturnsNumeralAsString(int input, string expected)
-        {
-            RomanConverter converter = new RomanConverter();
-
-            string actual = converter.Convert(input);
-
-            Assert.Equal(expected, actual);
-        }
-
-        [Theory]
-        [InlineData(4000, 4)]
-        [InlineData(4846, 4)]
-        [InlineData(846, 1)]
-        [InlineData(398, 3)]
-        [InlineData(76, 1)]
-        [InlineData(44, 4)]
-        [InlineData(7, 7)]
-        public void SplitNumeralValues_WhenGivenInt_ReturnsNumberOfNumeralOccurrences(int input, int expected)
-        {
-            RomanConverter converter = new RomanConverter();
-
-            int actual = converter.SplitNumeralValues(input);
-
-            Assert.Equal(expected, actual);
-        }
+        
     }
 }
